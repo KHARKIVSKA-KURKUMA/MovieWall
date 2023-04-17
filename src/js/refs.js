3d009babb6a142386df10142011950ab54c33de0
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-04-17 16:28:40 +0300

    add js-style

--- src/js/refs.js
+++ src/js/refs.js
@@ -30,7 +30,7 @@ export const refs = {
   form: document.querySelector('.search'),
   listEl: document.querySelectorAll('.js-btn-list'),
   libraryEl: document.querySelector('.library'),
-  library: document.querySelector('.library-btn'),
+  library: document.querySelector('.js-library'),
   activeClass: 'nav-list__button--active',
   home: document.querySelector('.js-home'),
   targetsList: {
