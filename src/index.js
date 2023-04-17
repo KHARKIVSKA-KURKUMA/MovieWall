17f6bf5b5c8f8b984f8bad403ea2b1b6f9d07aa6
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-04-17 15:51:14 +0300

    comment location reload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './js/watchedLibrary'
+import './js/watchedLibrary';
 import { refs } from './js/refs';
 import { renderPopularMovies } from './js/renderPopularPoster';
 import { onSubmit } from './js/renderFilmByName';
--- src/js/watchedLibrary.js
+++ src/js/watchedLibrary.js
@@ -1,49 +1,49 @@
-import { refs } from "./refs";
-import { fetchMovieForWatched } from "./fetchMovies";
+import { refs } from './refs';
+import { fetchMovieForWatched } from './fetchMovies';
 import notAvailablePoster from '../images/poster-not-available.jpg';
 const isMovieInWatched = () => {
-    let watchedMovies = null;
-    try {
-        watchedMovies = JSON.parse(localStorage.getItem('Watched movies'));
-    } catch {
-        return;
-    }
-    return watchedMovies;
-}
+  let watchedMovies = null;
+  try {
+    watchedMovies = JSON.parse(localStorage.getItem('Watched movies'));
+  } catch {
+    return;
+  }
+  return watchedMovies;
+};
 const watchedBtn = document.querySelector('.button-watched');
 const galleryContainer = document.querySelector('.gallery-container');
 let watchedMovies = isMovieInWatched();
 
-if (
-  (watchedMovies == null || watchedMovies.length === 0) 
-) {
-   clearLibrary();
+if (watchedMovies == null || watchedMovies.length === 0) {
+  clearLibrary();
 } else if (watchedMovies.length > 0) {
   watchedBtn.classList.add('is-active');
   renderLibrary(watchedMovies);
 }
 function renderPoster() {
-   galleryContainer.insertAdjacentHTML('beforeend', createPoster());
- }
+  galleryContainer.insertAdjacentHTML('beforeend', createPoster());
+}
 function renderLibrary(movies) {
   for (let i = 0; i < movies.length; i += 1) {
     fetchMovieForWatched(movies[i]).then(data => {
-      refs.homeGalleryList.insertAdjacentHTML('beforeend', createLibraryMovieItem(data));
+      refs.homeGalleryList.insertAdjacentHTML(
+        'beforeend',
+        createLibraryMovieItem(data)
+      );
     });
   }
 }
 
 const onWatchedBtnClick = event => {
-    watchedBtn.classList.add('is-active');
-   
-   
+  watchedBtn.classList.add('is-active');
+
   clearLibrary();
 
   watchedMovies = isMovieInWatched();
 
   if (watchedMovies == null || watchedMovies.length === 0) {
-    galleryContainer.innerHTML= '';
-    renderPoster()
+    galleryContainer.innerHTML = '';
+    renderPoster();
   } else if (watchedMovies.length > 0) {
     clearLibrary();
     renderLibrary(watchedMovies);
@@ -67,9 +67,7 @@ function createPoster() {
       `;
 }
 function createLibraryMovieItem(data) {
-  if (
-    (watchedMovies == null || watchedMovies.length === 0)
-  ) {
+  if (watchedMovies == null || watchedMovies.length === 0) {
     return;
   }
   const {
@@ -84,14 +82,16 @@ function createLibraryMovieItem(data) {
     vote_average,
     vote_count,
   } = data;
-const posterSrc = poster_path
-        ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+  const posterSrc = poster_path
+    ? `https://image.tmdb.org/t/p/w500/${poster_path}`
     : notAvailablePoster;
-  const genresMovies = data.genres.map(element => element.name)
-   const genresMovie = genresMovies.join(', ') ? genresMovies.slice(0, 2).join(', ') : 'Unknown genres';
+  const genresMovies = data.genres.map(element => element.name);
+  const genresMovie = genresMovies.join(', ')
+    ? genresMovies.slice(0, 2).join(', ')
+    : 'Unknown genres';
   const movieDate = release_date ?? null;
   const movieYear = movieDate ? movieDate.slice(0, 4) : 'Unknown year';
-    return `
+  return `
       <li class="filmcard">
   <a href="" class="filmcard-link link" data-id="${id}">
     <img class="filmcard-img"  src="${posterSrc}" alt="${title}"loading="lazy"/>
@@ -106,9 +106,7 @@ const posterSrc = poster_path
 
 watchedBtn.addEventListener('click', onWatchedBtnClick);
 
-
-
-const homeBtn = document.querySelector('.nav-list__button')
+const homeBtn = document.querySelector('.nav-list__button');
 homeBtn.addEventListener('click', e => {
- location.reload()
-})
+  // location.reload();
+});
